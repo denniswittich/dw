beed0ab0b63e3f26733489551852721fa446ab4b
Dennis Wittich <wittich@ipi.uni-hannover.de>
2018-11-21 16:24:28 +0100

    upgraded to v 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dw",
-    version="0.0.1",
+    version="0.0.2",
     author="Dennis Wittich",
     author_email="denniswittich@hotmail.de",
     description="Mixed tools",
